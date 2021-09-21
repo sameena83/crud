35ba0bcc56d437c6ea6c8432be3db775cc967035
sameena <sameenah.tm@gmail.com>
2021-09-21 09:21:05 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import React from 'react'
+
+import Restaurant from './components/Basics/Restaurant';
+
+function App() {
+  return (
+    <div>
+      <Restaurant/>
+    </div>
+  )
+}
+
+export default App
+
